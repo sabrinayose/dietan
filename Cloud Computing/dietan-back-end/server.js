1404ecc2773ff819ec56f8a72be2349fbd7e7357
Sabrina Yose Amelia <sabrina.amelia@student.matanauniversity.ac.id>
2023-06-09 04:11:35 +0700

    Update server.js

--- Cloud Computing/dietan-back-end/server.js
+++ Cloud Computing/dietan-back-end/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import foodsRoutes from './routes/foods.js';
-import user_daily_mealRoutes from './routes/user_daily_meals.js'
-import user_whsRoutes from './routes/user_whs.js'
-import db from './connection.js';
-import response from './response.js';
+import user_daily_mealRoutes from './routes/user_daily_meals.js';
+import user_whsRoutes from './routes/user_whs.js';
+import userRoutes from './routes/users.js';
+import user_medical_historyRoutes from './routes/user_medical_histories.js';
 
 const app = express();
 const PORT = 5000;
@@ -12,38 +12,15 @@ const PORT = 5000;
 app.use(bodyParser.json());
 
 app.use('/foods', foodsRoutes);
+app.use('/users', userRoutes);
 app.use('/user_daily_meals', user_daily_mealRoutes);
 app.use('/user_whs', user_whsRoutes);
+app.use('/user_medical_histories', user_medical_historyRoutes);
 
 app.get('/', (req, res) => {
     console.log('[TEST]!');
 
     res.send("Hello from Homepage");
 } );
-
-// app.get('/foodupdate/:foodId', (req, res) => {
-//     const { foodName, calories } = req.body;
-//     const { foodId } = req.params;
-//     const sql = `UPDATE food SET foodName = '${foodName}', calories = '${calories}' WHERE foodId = ${foodId};`;
-    
-//     db.query(sql, (error, result) => {
-//         // console.log(result);
-//         // response(200, result, "Food successfully updated", res);
-//         if (error) response(500, "invalid", "error", res);
-
-//         if (result?.affectedRows) {
-//             const data = {
-//                 isSuccess: result.affectedRows,
-//                 message: result.message,
-//             }
-//             response(200, data, "Food successfully updated", res);
-//         } else {
-//             response(404, "user not found", "error", res);
-//         }
-//     })
-//     // console.log("Hi");
-// })
-
-
 
 app.listen(PORT, () => console.log(`Server running on port : http://localhost:${PORT}`));
